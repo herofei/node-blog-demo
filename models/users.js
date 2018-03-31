40d53dea0c15797597a5aa85a02361546899d00a
Mat He <mat.he@qq.com>
2018-03-31 22:46:55 +0800

    signin

--- models/users.js
+++ models/users.js
@@ -4,5 +4,15 @@ module.exports = {
     // 注册一个用户
     create(user) {
         return User.create(user).exec();
+    },
+
+    // 通过用户名获取用户信息
+    getUserByName (name) {
+        return User
+            .findOne({
+                name: name
+            })
+            .addCreatedAt()
+            .exec();
     }
 };
--- routes/signout.js
+++ routes/signout.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const checkLogin  = require('../middlewares/check').checkLogin;
+const checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, (req, res, next) => {
-    res.send('登出');
+  // 清空 session 中用户信息
+  req.session.user = null;
+  req.flash('success', '登出成功');
+  // 登出成功后跳转到主页
+  res.redirect('/articles');
 });
 
 module.exports = router;
